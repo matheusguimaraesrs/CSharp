19be93dad6e28a7bf9dabfce03a01524515e89c1
Matheus Guimarães Castiglioni <Matheus Guimaraes@Gondim.local>
2025-12-09 15:59:13 -0300

    exe Hotel

---
+++ Hotel/Hospede.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Hospede
+    {
+        public string Nome {  get; private set; }
+        public string Email { get; private set; }
+        public int Quarto { get; private set; }
+
+        public Hospede(string nome, string email, int quarto) 
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public void QuartosOcupados()
+        {
+            Console.WriteLine($"{Quarto}: {Nome}, {Email}");
+          
+        }
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+
+            Hospede[] hospedes = new Hospede[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}º hospede");
+                Console.Write($"Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Email: ");
+                string email = Console.ReadLine();
+                Console.Write($"Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                hospedes[i] = new Hospede(nome, email, quarto);
+                Console.Clear();
+            }
+            Console.WriteLine("Quartos Ocupados");
+            for (int i = 0; i < n; i++)
+                hospedes[i].QuartosOcupados();
+        }
+    }
+}
