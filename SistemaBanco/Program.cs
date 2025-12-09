b806084d2fa3d75399b20d7f7320dafd7f8c1601
Matheus Guimarães Castiglioni <Matheus Guimaraes@Gondim.local>
2025-12-09 12:03:55 -0300

    Exercicio OOP

---
+++ SistemaBanco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SistemaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario;
+
+            Console.Write("Informe o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Informe o titular: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial? (s/n)");
+            char opcao = char.Parse(Console.ReadLine());
+            if (opcao == 's' || opcao == 'S')
+            {
+                Console.WriteLine("Informe o valor:");
+                double saldo = int.Parse(Console.ReadLine());
+                usuario = new Usuario(conta, titular, saldo);
+            }
+            else
+                usuario = new Usuario(conta, titular);
+
+            Console.WriteLine("Dados da conta:\n" + usuario);
+            
+            Console.WriteLine("\nInsira o novo depósito:");
+            double deposito = double.Parse(Console.ReadLine());
+            usuario.Deposito(deposito);
+            Console.WriteLine("Dados atualizados:\n" + usuario);
+
+            Console.WriteLine("Insira o valor do saque:");
+            double saque = double.Parse(Console.ReadLine());
+            usuario.Saque(saque);
+            Console.WriteLine("Dados atualizados:\n" + usuario);
+        }
+    }
+}
---
+++ SistemaBanco/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBanco
+{
+    internal class Usuario
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Usuario(int conta, string titular)
+        {
+            while (conta < 1000 || conta > 9999)
+            {
+                Console.WriteLine("A conta deve conter 4 dígitos!");
+                Console.WriteLine("Informe o número da conta: ");
+                conta = int.Parse(Console.ReadLine());
+            }
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public Usuario(int conta, string titular, double saldo) : this(conta, titular)
+        {
+            Deposito(saldo);
+        }
+
+        public override string ToString() 
+        {
+            return "Conta: " 
+                + Conta + ", Titular: " 
+                + Titular + ", Saldo: R$" 
+                + Saldo.ToString("F2");
+        }
+
+        public void Deposito(double deposito)
+        {
+            if (deposito != null)
+                Saldo += deposito; 
+        }
+        public void Saque(double saque)
+        {
+            if (Saldo != null)
+                Saldo -= (saque + 5.0);
+        }
+    }
+}
