78b1cfcdff9a662e6c64bfdab53be583ae108144
Matheus Guimarães Castiglioni <Matheus Guimaraes@Gondim.local>
2025-12-22 08:26:56 -0300

    heranca

---
+++ Heranca/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interesRate) : base(number, holder, balance)
+        {
+            InterestRate = interesRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2;
+        }
+    }
+}
---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account conta1 = new Account(1001, "alex", 500);
+            Account conta2 = new SavingsAccount(1002, "Maria", 500, 0.1);
+
+            conta1.Withdraw(10);
+            conta2.Withdraw(10);
+            Console.WriteLine(conta1.Balance);
+            Console.WriteLine(conta2.Balance);
+        }
+    }
+}
